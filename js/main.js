c48a87f7bcb0cfbaae13a84e6c97d540ce1d5afd
jose santiago vazquez <guerreiro1ggg@gmail.com>
2022-07-31 11:18:34 -0500

    agregando bucle while

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
 let nombre = prompt("¡Ingrese su nombre!, para tener una pizza personalizada")
-alert("¡Te damos la bienvenida a joseph´sPIZZA,  "+ nombre + "!")
-let promocion =  prompt("¡Hay una promocion que no te la puedes perder! '\n' ¿Quieres participar? '\n' inserte un 'si'o 'no' " )
-let respuesta = promocion.toLowerCase()
-if (respuesta == "si"){
-    let pregunta = parseInt(prompt("¡Ingresa tu número de la suerte para ver la promo!, elige entre el 1 y 5"))
-       console.log(darPromo(pregunta))
+alert("¡Te damos la bienvenida a JOSEPH´s PIZZA,  "+ nombre.toUpperCase() + "!")
+let promocion =  prompt("¡Hay una promocion que no te la puedes perder! \n ¿Quieres participar? '\n inserte un 'si'o 'no' " )
 
-    function darPromo(pregu){
-        switch(pregu){
-            case 1:
-                return alert("La promo consiste en la compra de una Pizza grande te llevas una gaseso mediana ¡Gratis! ")
-            case 2:
-                return alert("la promo consiste en la compra de dos Pizzas grandes te llevas una gaseosa grande ¡Gratis!")
-            case 3:
-                return alert("la promo consiste en la compra de dos Pizzas Medianas te llevas una gaseosa mediana ¡Gratis!")
-            case 4:
-                return alert("la promo consiste en la compra de tres Pizzas grandes te llevas dos gaseosa grande ¡Gratis!")
-            case 5:
-                return alert("la promo consiste en la compra de dos Pizzas chicas te llevas una gaseosa mediana ¡Gratis!")
-            default:
-                return alert("¡Suerte para la proxima!")
+
+function preguntas(){
+    
+        let pregunta = parseInt(prompt("¡Ingresa tu número de la suerte para ver la promo!, elige entre el 1 y 5"))
+        console.log(darPromo(pregunta))
+        alert("Bienvenido a JOSEPH´S PIZZA, " + nombre.toUpperCase())
+
+
+        function darPromo(pregu){
+            switch(pregu){
+                case 1:
+                    return alert("La promo consiste en la compra de una Pizza grande te llevas una gaseso mediana ¡Gratis! ")
+                case 2:
+                    return alert("la promo consiste en la compra de dos Pizzas grandes te llevas una gaseosa grande ¡Gratis!")
+                case 3:
+                    return alert("la promo consiste en la compra de dos Pizzas Medianas te llevas una gaseosa mediana ¡Gratis!")
+                case 4:
+                    return alert("la promo consiste en la compra de tres Pizzas grandes te llevas dos gaseosa grande ¡Gratis!")
+                case 5:
+                    return alert("la promo consiste en la compra de dos Pizzas chicas te llevas una gaseosa mediana ¡Gratis!")
+                default:
+                    return alert("¡Suerte para la proxima!")
+            }
+
+
         }
-    }
-}else{
-    alert("¡Gracias por su visita!")
+    
 }
-alert("¡Bienvenido  " + nombre + " !")
+
+while (promocion){
+    if(promocion == "no"){
+        let pregunta3 = prompt("quieres salir?")
+        if(pregunta3 === "no"){
+            preguntas()
+            break
+        }else{
+            alert("Bienvenido a JOSEPH´S PIZZA, " + nombre.toUpperCase())
+            break
+        }
+        
+    }else{
+        preguntas()
+        break
+    }
+}
