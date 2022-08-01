45c83b67cb1cf5fbd893c2092ced9b3f3eac4cb4
jose santiago vazquez <guerreiro1ggg@gmail.com>
2022-07-31 23:23:51 -0500

    agregando ciclo while

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let nombre = prompt("¡Ingrese su nombre!, para tener una pizza personalizada")
-alert("¡Te damos la bienvenida a JOSEPH´s PIZZA,  "+ nombre.toUpperCase() + "!")
-let promocion =  prompt("¡Hay una promocion que no te la puedes perder! \n ¿Quieres participar? '\n inserte un 'si'o 'no' " )
+alert("¡Te damos la bienvenida a JOSEPH´S PIZZA,  "+ nombre.toUpperCase() + "!")
+let promocion =  prompt("¡Hay una promocion que no te la puedes perder! \n ¿Quieres participar? \n inserte un \n Si \n o \n No " )
 let promocion2 = promocion.toLowerCase()
 
 function preguntas(){
     
-        let pregunta = parseInt(prompt("¡Ingresa tu número de la suerte para ver la promo!, elige entre el 1 y 5"))
+        let pregunta = parseInt(prompt("¡Ingresa tu número de la suerte para ver la promo!, elige entre el 1 y 10"))
         console.log(darPromo(pregunta))
         alert("Bienvenido a JOSEPH´S PIZZA, " + nombre.toUpperCase())
 
@@ -13,15 +13,20 @@ function preguntas(){
         function darPromo(pregu){
             switch(pregu){
                 case 1:
+                case 6:
                     return alert("La promo consiste en la compra de una Pizza grande te llevas una gaseso mediana ¡Gratis! ")
                 case 2:
-                    return alert("la promo consiste en la compra de dos Pizzas grandes te llevas una gaseosa grande ¡Gratis!")
+                case 7:
+                    return alert("La promo consiste en la compra de dos Pizzas grandes te llevas una gaseosa grande ¡Gratis!")
                 case 3:
-                    return alert("la promo consiste en la compra de dos Pizzas Medianas te llevas una gaseosa mediana ¡Gratis!")
+                case 8:
+                    return alert("La promo consiste en la compra de dos Pizzas medianas te llevas una gaseosa mediana ¡Gratis!")
                 case 4:
-                    return alert("la promo consiste en la compra de tres Pizzas grandes te llevas dos gaseosa grande ¡Gratis!")
+                case 9:
+                    return alert("La promo consiste en la compra de tres Pizzas grandes te llevas dos gaseosa grande ¡Gratis!")
                 case 5:
-                    return alert("la promo consiste en la compra de dos Pizzas chicas te llevas una gaseosa mediana ¡Gratis!")
+                case 10:
+                    return alert("La promo consiste en la compra de dos Pizzas chicas te llevas una gaseosa mediana ¡Gratis!")
                 default:
                     return alert("¡Suerte para la proxima!")
             }
@@ -33,7 +38,7 @@ function preguntas(){
 
 while (promocion2){
     if(promocion2 == "no"){
-        let pregunta3 = prompt("quieres salir?")
+        let pregunta3 = prompt("¿Estas seguro de querer salir? \n inserte un \n Si \n o \n  No")
         let pregunt = pregunta3.toLowerCase()
         if(pregunt === "no"){
             preguntas()
