b161fa30d61784b7ea2c5a79a1edb4510c88f6de
jose santiago vazquez <guerreiro1ggg@gmail.com>
2022-09-12 00:54:58 -0500

    agregando carrito y formulario de compra

--- js/eventos.js
+++ js/eventos.js
@@ -1,20 +1,54 @@
-// btnSumar.addEventListener('click', (e)=>{
-//     numero++
-//     contador.innerHTML = numero
-//     console.log(e.target)
-// })
-// btnRestar.addEventListener('click', (e)=>{
-//     if(numero == 0){}
-//     else{
-//         numero--
-//         contador.innerHTML = numero
-//     }
-//     console.log(e.target)
-// })
-
 verCarrito.addEventListener('click',()=>{
     ventanaCarrito.classList.add('ver')
 })
+
 cerrarCarrito.addEventListener('click',()=>{
     ventanaCarrito.classList.remove('ver')
 })
+
+btnPagar.addEventListener('click', ()=>{
+    if(carrito.length > 0){
+        formulario.classList.add("activeDos")
+    }else{
+        Swal.fire({
+            icon:'warning',
+            toast:true,
+            text:'No tienes productos que comprar',
+            timer:2000,
+            showConfirmButton: false,
+        })
+    }
+    
+})
+
+btnGuardar.addEventListener('click', ()=>{
+    datosInput()
+    formulario.classList.remove("activeDos")
+})
+
+btnContinuar.addEventListener('click',()=>{
+    if(carrito.length > 0  && arrayInput.length > 0){
+        procesandoCompra()
+        return new Promise((resolve)=>{
+            setTimeout(() =>{
+                resolve(ventanaCarrito.classList.remove('ver'))
+            },3000)
+        })
+
+    }
+    else if(carrito.length == 0 || arrayInput.length == 0 ){
+        Swal.fire({
+            icon:'warning',
+            toast:true,
+            text:'Por favor verifica tus productos en el carrito o que el formulario este lleno',
+            timer:2200,
+            showConfirmButton: false,
+        })
+        
+    }
+   
+})
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function crearCards(articulos){
       <p class="precio" id="precio">
        $ ${articulos.precio}
       </p>
-      <button class="ver" id="ver">
+      <button class="ver" id="ver${articulos.id}">
        ver
       </button>
        <button class="agregarCarrito" id="agregarCarrito${articulos.id}">
@@ -19,8 +19,8 @@ function crearCards(articulos){
     </div>`
 }    
 
-const renderizarCards = async ()=>{
-    await fetch(URL)
+const renderizarCards = ()=>{
+         fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 almacenar = data
@@ -28,11 +28,33 @@ const renderizarCards = async ()=>{
                     renderizarProducto += crearCards(articulos)
                 })
                 cajaCards.innerHTML = renderizarProducto
+                botonAgregar()
+                contadorProductos()
+                verInfo()
+
             })
             .catch((error)=>{
                 cajaCards.innerHTML = cargarError(error)
             })
-            botonAgregar()
+}
+
+function verInfo(){
+    almacenar.forEach(prod =>{
+        document.getElementById(`ver${prod.id}`).addEventListener('click', (e)=>{
+            Swal.fire({
+                imageUrl:prod.img,
+                text:prod.descripcion,
+                width:'70%',
+                padding:'10px 10px 30px 10px',
+                allowOutsideClick:false,
+                alowEscapekey:false,
+                allowEnterkey:false,
+                stopkeydownPropagation:false,
+                confirmButtonColor: '#000',
+                confirmButtonText: 'Salir',
+            })
+        })
+    })
 }
 
 const cargando = ()=>{
@@ -43,13 +65,12 @@ const cargando = ()=>{
         didOpen:()=>{
             Swal.showLoading()
         },
-        timer:2500,
+        timer:2100,
     })
     return new Promise((resolve, reject)=>{
         setTimeout(() => {
             resolve(renderizarCards())
-        }, 2500);
-
+        }, 2100);
     })
 
 }
@@ -63,10 +84,12 @@ const cargarError = ()=>{
         }
       )
 }
+
 function botonAgregar(){
     almacenar.forEach(prod =>{
         document.getElementById(`agregarCarrito${prod.id}`).addEventListener('click', ()=>{
             agregarCarrito(prod)
+            contadorProductos()
             alertAgregar()
         })
     })
@@ -78,11 +101,19 @@ function agregarCarrito(prod){
         prod.cantidad = 1
         carrito.push(prod)
     }else{
-        let prodFind = carrito.find((productoFind) => productoFind.id == prod.id)
+        let prodFind = carrito.find((productoFind) => productoFind.id === prod.id)
         prodFind.cantidad++
     }
     presentandoCarrito()
+    
 }
+
+function precioTotal(){
+    const precioT =carrito.reduce((acc, {cantidad,precio}) => acc + cantidad * precio,0)
+    totalPagar.innerText = precioT
+    console.log(precioT)
+}
+
 function presentandoCarrito(){
     seMuestraProducto.innerHTML = ""
     carrito.forEach(prod =>{
@@ -90,36 +121,133 @@ function presentandoCarrito(){
         <p>${prod.nombre}</p>
         <div class="precioUnitario">
           <span>${prod.precio}</span>
-          <span>Total</span>
         </div>
         <p>${prod.cantidad}</p>
         <p class="eliminar" id="eliminar${prod.id}"><i class="fa-solid fa-trash-can"></i></p>
       </div>`
     })
     localStorage.setItem("carrito",JSON.stringify(carrito))
-    
+    precioTotal()
+    eliminarProducto()   
     
 }
+
+
     
+
+function eliminarProducto(){
+    carrito.forEach(element =>{
+        document.getElementById(`eliminar${element.id}`).addEventListener('click',(e)=>{
+            console.log(e.target)
+            carrito = carrito.filter((productosFilter) => productosFilter.id !== element.id)
+            presentandoCarrito()
+            contadorProductos()
+            alertEliminar()
+        })
+    })
+}
+
+function datosInput(){
+    var valuarInputs = document.querySelectorAll(".datos")
+    datosValuar = [].map.call(valuarInputs, (datosInput)=>{
+        arrayInput.push(datosInput.value)
+        mostramosDatos()
+    })
+    console.table(arrayInput)
+
+    
+}
+
+function mostramosDatos(){
+    arrayInput.innerHTML =""
+    arrayInput.forEach(() =>{
+        cardInfor.innerHTML = `
+        <h1>Datos</h1>
+        <span>Datos del usuario</span>
+        <p>Nombre: ${arrayInput[0]}</p>
+        <p>Apellido: ${arrayInput[1]}</p>
+        <p>Correo electrónico: ${arrayInput[2]}</p>
+        <p>Cel: ${arrayInput[3]}</p>
+        <span>Domicilio de entrega</span>
+        <p>Calle: ${arrayInput[4]}</p>
+        <p>CP: ${arrayInput[5]}</p>
+        <p>Colonia: ${arrayInput[6]}</p>
+        <p>Estado: ${arrayInput[7]}</p>
+        <p>Municipio: ${arrayInput[8]}</p>
+        <span>Forma de pago</span>
+        <p>Num Tarjeta: ${arrayInput[9]}</p>
+        <p>Nombre: ${arrayInput[10]}</p>
+        <p>Fecha: ${arrayInput[11]}</p>
+        <p>CVV: ${arrayInput[12]}</p>
+       
+                                `
+    })
+    localStorage.setItem("datos",JSON.stringify(arrayInput))
+   
+}
+
+const procesandoCompra = ()=>{
+    Swal.fire({
+        toast:true,
+        title:'PROCESANDO COMPRA',
+        showConfirmButton: false,
+        didOpen:()=>{
+            Swal.showLoading()
+        },
+        timer:2100,
+    })
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(eliminarCarrito())
+        }, 2100);
+    })
+}
+
+function eliminarCarrito(){
+    cardInfor.innerHTML =""
+    carrito.length = 0
+    localStorage.clear("datos")
+    presentandoCarrito()
+    contadorProductos()
+    alerExito()
+}
+
+function alerExito(){
+    Swal.fire({
+        toast:true,
+        icon:'success',
+        text:'COMPRA EXITOSA',
+        showConfirmButton: false,
+        timer:3000,
+    })
+}
+
 function alertEliminar(){
     Swal.fire({
         toast:true,
         icon:'success',
-        position: 'bottom-end',
+        position: 'bottom-start',
         showConfirmButton: false,
-        timer: 1500,
+        timer: 1000,
         text:'Eliminado',
     })
 }
+
 function alertAgregar(){
     Swal.fire({
         toast:true,
         icon:'success',
-        position: 'top-start',
+        position: 'bottom-start',
         showConfirmButton: false,
-        timer: 1500,
+        timer: 1000,
         text:'Agregado al carrito',
     })
 }
 
+function contadorProductos(){
+    contadorCarrito.innerText = carrito.length
+}
+
+presentandoCarrito()
+mostramosDatos()
 cargando()
